abcf9587745840903d30f957c9b655db99c3ce66
Cameron Dubas <camerondubas@gmail.com>
2016-08-26 00:49:59 -0700

    Update simplifying-fractions.js

--- 277-easy/simplifying-fractions.js
+++ 277-easy/simplifying-fractions.js
@@ -1,7 +1,7 @@
+let gcd = (val1, val2) => val1 % val2 === 0 ? val2 : gcd(val2, val1 % val2);
+
 function simplify(input) {
   let [numerator, denominator] = input.split(' ').map(i => parseInt(i));
   let divisor = gcd(numerator, denominator);
   return `${numerator/divisor} ${denominator/divisor}`;
 }
-
-function gcd(val1, val2) => val1 % val2 === 0 ? val2 : gcd(val2, val1 % val2);
